1f38d16731190de71d0ba0f03b0fda994f4b3abb
Randy Park <parkrandy@gmail.com>
2024-02-09 16:20:07 -0500

    Add Kernel Memory Summary and Utilities

---
+++ 07.kernelmemory.summary/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.AI.OpenAI;
+
+
+// Common Setting: Get Settings from Settings.json
+var (useAzureOpenAI, chatDeployment, azureEndpoint, apiKey, orgId) = Settings.LoadFromFile();
+
+// Section 2: Create a Semantic Kernel with local memory
+var embbeddingDeplpoyment = Settings.EmbeddingModelKey();
+
+var azureOpenAIEmbeddingConfig = new AzureOpenAIConfig
+{
+    APIKey = apiKey,
+    Deployment = embbeddingDeplpoyment,
+    Endpoint = azureEndpoint,
+    APIType = AzureOpenAIConfig.APITypes.EmbeddingGeneration,
+    Auth = AzureOpenAIConfig.AuthTypes.APIKey
+};
+
+var azureOpenAITextConfig = new AzureOpenAIConfig
+{
+    APIKey = apiKey,
+    Deployment = chatDeployment,
+    Endpoint = azureEndpoint,
+    APIType = AzureOpenAIConfig.APITypes.ChatCompletion,
+    Auth = AzureOpenAIConfig.AuthTypes.APIKey
+};
+
+// Kernel Memory
+// https://github.com/microsoft/kernel-memory
+
+var kernelMemory = new KernelMemoryBuilder()
+    .WithAzureOpenAITextGeneration(azureOpenAITextConfig)
+    .WithAzureOpenAITextEmbeddingGeneration(azureOpenAIEmbeddingConfig)
+    .Build<MemoryServerless>();
+
+var dataDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Documents");
+var dataFiles = Directory.EnumerateFiles(dataDirectory, "*.*", SearchOption.AllDirectories);
+await kernelMemory.ImportDocumentAsync(new Document("summaryDocument")
+            .AddFiles(dataFiles),
+            steps: Constants.PipelineOnlySummary);
+
+var results = await kernelMemory.SearchSummariesAsync(filter: MemoryFilters.ByDocument("summaryDocument"));
+
+foreach (var result in results)
+{
+    Console.WriteLine($"== {result.SourceName} summary ==\n{result.Partitions.First().Text}\n");
+}
---
+++ 07.kernelmemory.utilities/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.KernelMemory.DataFormats.Office;
+using Microsoft.KernelMemory.DataFormats.Pdf;
+
+var dataDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Documents");
+var dataFiles = Directory.EnumerateFiles(dataDirectory, "*.*", SearchOption.AllDirectories);
+
+var pptxFiles = dataFiles.Where(file => file.EndsWith(".pptx"));
+foreach (var pptxFile in pptxFiles)
+{
+    var text = new MsPowerPointDecoder().DocToText(pptxFile,
+        withSlideNumber: true,
+        withEndOfSlideMarker: false,
+        skipHiddenSlides: true);
+    Console.WriteLine(text);
+}
+
+Console.ReadLine();
+var pdfFiles = dataFiles.Where(file => file.EndsWith(".pdf"));
+foreach (var pdfFile in pdfFiles)
+{
+    var pages = new PdfDecoder().DocToText(pdfFile);
+    foreach (var page in pages)
+    {
+        Console.WriteLine(page.Text);
+    }
+}
